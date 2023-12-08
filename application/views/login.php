c56f3b6e0b749725cc9bc2a8b59c4ac461c1b7d4
jiangruigoh <150787408+isyourjr@users.noreply.github.com>
2023-12-08 11:41:10 +0800

    Update login.php

--- application/views/login.php
+++ application/views/login.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="<?php echo base_url("assets/bootstrap/css/bootstrap.min.css"); ?>">
-  <link rel="stylesheet" href="<?php echo base_url("assets/bootstrap/css/bootstrap-drawer.min.css"); ?>">
+  <!-- <link rel="stylesheet" href="<?php echo base_url("assets/bootstrap/css/bootstrap-drawer.min.css"); ?>"> -->
   <link rel="icon" type="image/png" href="<?php echo base_url('asset/dist/img/rexbridge.JPG');?>" >
 <style>
       /* Set black background color, white text and some padding */
